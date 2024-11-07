89e7642f2625eb2cd0e6113809b49d84ded73841
A-lexiss <1843038@hc.ac.je>
2024-11-07 11:10:27 +0000

    comment planning

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
-// javascript
-
-alert(console.log);
+// Display a form to the user
+// Include input fields for the user's email address, level, and hours of study
+// Capture user's input on form submission
+//get the users email address - useremail (string)
+//get the users level - userlevel (string)
+//get users hours of study - userhours (number)
+// Validate the user's input
+// Check if the user has selected a level - check a level has been selected, check against the allowed levels,
+// Check if the user has provided an email address - check a email is present, is it valid?, does it have an @
+// Check if the user has specified at least one hour of study - =>1 hours selected,
+// Check if the number of hours requested is within the allowed range - <= maximum number for the level
+// Calculate the total cost
+// Display the total cost to the user
